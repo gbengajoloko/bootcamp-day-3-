8a0cc692da27d6a3df7b306265b85fce68fa1df8
gbengajoloko <gbengajoloko@gmail.com>
2017-03-15 17:11:56 +0100

    added comments

--- app/array.js
+++ app/array.js
@@ -1,53 +1,54 @@
-Array.prototype.toTwenty = function () {
+Array.prototype.toTwenty = function () {//creates an array of 1 to 20
   var arr = []
   for (var i = 1; i <= 20; i++) {
     arr.push(i)
   }
   return arr
+
 }
-Array.prototype.toForty = function () {
+Array.prototype.toForty = function () {//creates an array of 2 to 40 at intervals of 2
   var arr = []
   for (var i = 2; i <= 40; i += 2) {
     arr.push(i)
   }
   return arr
 }
-Array.prototype.toOneThousand = function () {
+Array.prototype.toOneThousand = function () {// create an array of 10 to 10000 at intervals of 10
   var arr = []
   for (var i = 10; i <= 1000; i += 10) {
     arr.push(i)
   }
   return arr
 }
-Array.prototype.search = function (d) {
+Array.prototype.search = function (d) {//search the array for the missing number
   var startIndex = 0
   var len = this.length,
     stopIndex = this.length - 1,
-    mid = Math.floor((stopIndex + startIndex) / 2),
+    mid = Math.floor((stopIndex + startIndex) / 2), //search starts at the mid point
     count = 0
 
   while (this[mid] != d && startIndex < stopIndex) {
-    if (d == this[startIndex]) {
+    if (d === this[startIndex]) {//checks if value is at the first index
       mid = startIndex
       break
     }
-    if (d == this[stopIndex]) {
+    if (d ===this[stopIndex]) {//checks if value is at last index
       mid = stopIndex
       break
     }
-    if (d < this[mid]) {
+    if (d < this[mid]) { //checks if value is less the value at the mid index
       stopIndex = mid - 1
-    } else if (d > this[mid]) {
+    } else if (d > this[mid]) {//checks if value is greater than the value at the mid index
       startIndex = mid + 1
     }
-    mid = Math.floor((stopIndex + startIndex) / 2)
-    count++
+    mid = Math.floor((stopIndex + startIndex) / 2)// recalculates the mid index
+    count++ //increases the value of count 
   }
 
       // make sure it's the right value
-  if (this[mid] !== d) {
+  if (this[mid] !== d) { //checks if value is found returns -1 if not found  
     return {'count': count, 'index': -1, 'length': len}
-  } else {
+  } else { //returns the right index if found
     return {'count': count, 'index': mid, 'length': len}
   }
 }
--- app/findMissing.js
+++ app/findMissing.js
@@ -1,10 +1,8 @@
 var exports = module.exports = {}
-exports.findMissing= function(array1,array2){
-	array3 = array1.concat(array2)
-	var missing = array3.filter(function(num) { return array3.indexOf(num) === array3.lastIndexOf(num)})
-   		if (missing[0]==null){
-   			return 0
-
-   		}
-   		else {return missing[0]}
+exports.findMissing = function (array1, array2) {
+  var array3 = array1.concat(array2) // adds the two array into a single array
+  var missing = array3.filter(function (num) { return array3.indexOf(num) === array3.lastIndexOf(num) })// filters out numbers that occurs twice
+  if (missing[0] == null) { // if number is undefined returns 0
+    return 0
+  } else { return missing[0] }// returs the missing number 
 }
